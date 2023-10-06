6ff6e5f4d2e7811bc89f12283a8f3d1a096a8487
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2023-10-06 12:51:54 +1000

    Make helm executable

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -8,6 +8,7 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+sudo chmod +x /usr/local/bin/helm
 
 # create local registry
 echo "-- Create docker network"
